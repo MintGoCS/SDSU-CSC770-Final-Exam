5e99782cc6a29305c3a1ffa929c15a0d9ca32426
Zhongyu Xie <164752555+MintGoCS@users.noreply.github.com>
2024-12-13 22:27:53 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ from Question01 import Person, BlindPeople, FullTimeMilitary, FullTimeStudent, N
 class TaxTest(unittest.TestCase):
     """Tax tests."""
     def test_mutation_value(self):
+        """
+        Test the tax calculation for mutation cases.
+        Test A taxpayer with negative number of children,
+        or negative number of income.
+        """
         test_cases = [
             # Format: (income, children)
             (20000, -1),  # children=-1
@@ -18,49 +23,148 @@ class TaxTest(unittest.TestCase):
                     NormalTaxpayer(income=income, children=children)
 
     def test_different_incomes(self):
+        """
+        Test the tax calculation for normal income cases,
+        including multiple boundary values.
+        The social security tax (6.5%) is required.
+        """
         test_cases = [
             # Format: (income, children, expected_tax)
+            # income: the income for a normal taxpayer.
+            # children: the number of children the normal taxpayer has.
+            # expected_tax: the expected total tax (income tax + social security tax)
 
+            # Case Group 1
             # Income range: 0 <= income <= 25,000
-            (0, 0, 0 * 0.1),
-            (20000, 1, 20000 * 0.08),
-            (25000, 2, 25000 * 0.06),
+            (0, 0, 0),
+            (20000, 1, 20000 * 0.08 + 20000 * 0.065),
+            (25000, 2, 25000 * 0.06 + 25000 * 0.065),
 
+            # Case Group 2
             # Income range: 25,001 <= income <= 55,000
-            (25001, 0, 25001 * 0.15),
-            (40000, 1, 40000 * 0.12),
-            (55000, 2, 55000 * 0.11),
+            (25001, 0, 25001 * 0.15 + 25001 * 0.065),
+            (40000, 1, 40000 * 0.12 + 40000 * 0.065),
+            (55000, 2, 55000 * 0.11 + 55000 * 0.065),
 
+            # Case Group 3
             # Income range: 55,001 <= income <= 105,000
-            (55001, 0, 55001 * 0.25),
-            (80000, 1, 80000 * 0.23),
-            (105000, 2, 105000 * 0.2),
+            (55001, 0, 55001 * 0.25 + 55001 * 0.065),
+            (80000, 1, 80000 * 0.23 + 80000 * 0.065),
+            (105000, 2, 105000 * 0.2 + 105000 * 0.065),
 
+            # Case Group 4
             # Income range: 105,001 <= income <= 205,000
-            (105001, 0, 105001 * 0.27),
-            (150000, 1, 150000 * 0.25),
-            (150000, 3, 150000 * 0.3),
+            (105001, 0, 105001 * 0.27 + 105001 * 0.065),
+            (150000, 1, 150000 * 0.25 + 150000 * 0.065),
+            (205000, 3, 205000 * 0.30 + 205000 * 0.065),
 
+            # Case Group 5
             # Income range: 205,001 <= income <= 255,000
-            (205001, 0, 205001 * 0.3),
-            (220000, 1, 220000 * 0.28),
-            (255000, 3, 255000 * 0.3),
+            (205001, 0, 205001 * 0.30 + 205001 * 0.065),
+            (220000, 1, 220000 * 0.28 + 220000 * 0.065),
+            (255000, 3, 255000 * 0.30 + 255000 * 0.065),
 
+            # Case Group 6
             # Income range: 255,001 <= income <= 405,000
-            (255001, 0, 255001 * 0.4),
-            (350000, 1, 350000 * 0.36),
-            (350000, 3, 350000 * 0.3),
+            (255001, 0, 255001 * 0.40 + 255001 * 0.065),
+            (350000, 1, 350000 * 0.36 + 350000 * 0.065),
+            (350000, 3, 350000 * 0.30 + 350000 * 0.065),
 
+            # Case Group 7
             # Income range: income >= 405,001
-            (800000, 0, 800000 * 0.5),
-            (800000, 1, 800000 * 0.5),
-            (800000, 3, 800000 * 0.5),
+            (800000, 0, 800000 * 0.5 + 800000 * 0.065),
+            (800000, 1, 800000 * 0.5 + 800000 * 0.065),
+            (800000, 3, 800000 * 0.5 + 800000 * 0.065),
         ]
 
         for income, children, expected_tax in test_cases:
             with self.subTest(income=income, children=children):
                 normal_taxpayer = NormalTaxpayer(income=income, children=children)
                 self.assertEqual(normal_taxpayer.calculate_tax(), expected_tax)
+
+    def test_full_time_students(self):
+        """
+        Test the tax calculation for full-time students.
+        Full-time students receive a 70% discount on income tax.
+        The social security tax (6.5%) is required.
+        """
+        test_cases = [
+            # Format: (income, children, expected_tax)
+            # income: the income for a full-time student.
+            # children: the number of children the full-time student has.
+            # expected_tax: the expected total tax (income tax + social security tax)
+
+            # Case Student 1
+            (0, 0, 0),
+
+            # Case Student 2
+            # expected_tax: income tax = 5000 * 10% (income <= 25,000, 0 child) * 70% (discount)
+            #               social security tax = 5000 * 6.5%
+            #               total tax = income tax + social security tax
+            (5000, 0, 5000 * 0.1 * 0.7 + 5000 * 0.065),
+
+        ]
+
+        for income, children, expected_tax in test_cases:
+            with self.subTest(income=income, children=children):
+                full_time_student = FullTimeStudent(income=income, children=children)
+                self.assertEqual(full_time_student.calculate_tax(), expected_tax)
+
+    def test_full_time_military(self):
+        """
+        Test the tax calculation for full-time military.
+        Full-time militaries receive a 75% discount on both income tax and social security tax.
+        The social security tax (6.5%) is required before discount applied.
+        """
+        test_cases = [
+            # Format: (income, children, expected_tax)
+            # income: the income for a full-time military.
+            # children: the number of children the full-time military has.
+            # expected_tax: the expected total tax (income tax + social security tax)
+
+            # Case Military 1
+            # expected_tax: income tax = 65000 * 20% (income <= 105,000, 3 children) * 75% (discount)
+            #               social security tax = 65000 * 6.5% * 75% (discount)
+            #               total tax = income tax + social security tax
+            (65000, 3, 65000 * 0.2 * 0.75 + 65000 * 0.065 * 0.75),
+
+            # Case Military 2
+            (37800, 0, 37800 * 0.15 * 0.75 + 37800 * 0.065 * 0.75),
+
+        ]
+
+        for income, children, expected_tax in test_cases:
+            with self.subTest(income=income, children=children):
+                full_time_military = FullTimeMilitary(income=income, children=children)
+                self.assertEqual(full_time_military.calculate_tax(), expected_tax)
+
+    def test_blind_people(self):
+        """
+        Test the tax calculation for blind people.
+        Blind people whose income are lower than 65,000 will not be tax but social security tax is required.
+        The social security tax is 6.5%.
+        """
+        test_cases = [
+            # Format: (income, children, expected_tax)
+            # income: the income for a blind people.
+            # children: the number of children the blind people has.
+            # expected_tax: the expected total tax (income tax + social security tax)
+
+            # Case Blind people 1
+            # expected_tax: income tax = 0
+            #               social security tax = 30,000 * 6.5%
+            #               total tax = income tax + social security tax
+            (30000, 1, 0 + 30000 * 0.065),
+
+            # Case Blind people 2
+            (125800, 0, 125800 * 0.27 + 125800 * 0.065),
+
+        ]
+
+        for income, children, expected_tax in test_cases:
+            with self.subTest(income=income, children=children):
+                blind_people = BlindPeople(income=income, children=children)
+                self.assertEqual(blind_people.calculate_tax(), expected_tax)
 
 
 if __name__ == '__main__':
